013865461fabb98fe34ec077474429680d24f2cd
Pedrulfa <artolapedro26@gmail.com>
2025-11-17 18:16:45 -0300

    Inicio de sesion funcionando

---
+++ app/controllers/sessions_controller.rb
@@ -0,0 +1,23 @@
+class SessionsController < ApplicationController
+  
+  def new
+    # solo muestra el formulario
+  end
+
+  def create
+    user = User.find_by(email: params[:email].downcase)
+    
+    if user && user.authenticate(params[:password])
+      session[:user_id] = user.id
+      redirect_to root_path, notice: "Sesión iniciada correctamente"
+    else
+      flash.now[:alert] = "Email o contraseña incorrectos"
+      render :new
+    end
+  end
+
+  def destroy
+    session[:user_id] = nil
+    redirect_to login_path, notice: "Sesión cerrada"
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,19 @@
 Rails.application.routes.draw do
+  get "sessions/new"
+  get "sessions/create"
+  get "sessions/destroy"
   
   get "up" => "rails/health#show", as: :rails_health_check
 
+  # Página principal
   root "home#index"
 
+  # Para gestionar usuarios
   resources :users, only: [:new, :create]
+
+  # Para iniciar sesión
+  get    '/login',  to: 'sessions#new'
+  post   '/login',  to: 'sessions#create'
+  delete '/logout', to: 'sessions#destroy'
+
 end
