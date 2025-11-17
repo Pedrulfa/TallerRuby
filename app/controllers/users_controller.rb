2b84ac4d2ac7231c192752aaaa1fb4cfc4dbb8d3
Pedrulfa <artolapedro26@gmail.com>
2025-11-17 17:18:25 -0300

    Registro funcionando con todas las validaciones

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,2 +1,24 @@
 class UsersController < ApplicationController
+  def new
+    @user = User.new
+  end
+
+  def create
+    @user = User.new(user_params)
+    
+    # role_id por defecto 
+    @user.role_id ||= 4
+    
+    if @user.save
+      redirect_to root_path, notice: "Usuario registrado correctamente"
+    else
+      render :new, status: :unprocessable_entity
+    end
+  end
+
+  private
+
+  def user_params
+    params.require(:user).permit(:name, :surname, :email, :password, :password_confirmation)
+  end
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -2,6 +2,10 @@ class User < ApplicationRecord
     belongs_to :role
     has_secure_password
 
-    validates :email, presence: true, uniqueness: true
+    validates :email, presence: { message: "No puede estar vacío" }, 
+                      uniqueness: { message: "Ya está en uso" }
+    validates :password, length: { minimum: 6, message: "Debe tener al menos 6 caracteres" }
+    validates :name, presence: { message: "No puede estar vacío" }
+    validates :surname, presence: { message: "No puede estar vacío" }
 
 end
