1b536bef7d885e8b63ce54ef985b93a5bccabdc9
Pedrulfa <artolapedro26@gmail.com>
2025-11-17 16:23:26 -0300

    Cambio de nombre de columna en roles

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[8.1].define(version: 2025_11_17_191605) do
+ActiveRecord::Schema[8.1].define(version: 2025_11_17_192203) do
   create_table "audios", force: :cascade do |t|
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
@@ -51,7 +51,7 @@ ActiveRecord::Schema[8.1].define(version: 2025_11_17_191605) do
 
   create_table "roles", force: :cascade do |t|
     t.datetime "created_at", null: false
-    t.string "type"
+    t.string "name"
     t.datetime "updated_at", null: false
   end
 
