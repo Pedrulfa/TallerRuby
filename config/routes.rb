74901542a82c78ff7786dc934229fc5d28eeb2e8
Pedrulfa <artolapedro26@gmail.com>
2025-11-17 10:47:26 -0300

    Renderizado a home y columna para eliminado logico en tabla Sale

--- config/routes.rb
+++ config/routes.rb
@@ -2,5 +2,6 @@ Rails.application.routes.draw do
   
   get "up" => "rails/health#show", as: :rails_health_check
 
+  root "home#index"
   get "/home" => "home#index", as: :home
 end
--- db/migrate/20251116212518_create_sales.rb
+++ db/migrate/20251116212518_create_sales.rb
@@ -3,6 +3,7 @@ class CreateSales < ActiveRecord::Migration[8.1]
     create_table :sales do |t|
       t.datetime :date
       t.decimal :total
+      t.boolean :completed, default: true     #Si la venta se cancela se pone en false
 
       t.timestamps
     end
