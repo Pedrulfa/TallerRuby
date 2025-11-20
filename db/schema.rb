7f3a90d9e8bc2ce71a1d132f4e6be8105604873f
¨Joaquin <fontana200514@gmail.com>
2025-11-20 18:20:50 -0300

    Correcion migracion de role y relacion con user

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[8.1].define(version: 2025_11_17_192203) do
+ActiveRecord::Schema[8.1].define(version: 2025_11_20_211648) do
   create_table "audios", force: :cascade do |t|
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
@@ -86,7 +86,7 @@ ActiveRecord::Schema[8.1].define(version: 2025_11_17_192203) do
     t.string "email"
     t.string "name"
     t.string "password_digest"
-    t.integer "role_id"
+    t.integer "role_id", null: false
     t.string "surname"
     t.datetime "updated_at", null: false
     t.index ["role_id"], name: "index_users_on_role_id"
