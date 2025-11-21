70c27a7886888f2085ea91d41936b443f44cb538
¨Joaquin <fontana200514@gmail.com>
2025-11-21 11:40:52 -0300

    Listado de productos backstore

--- app/controllers/backstore/products_controller.rb
+++ app/controllers/backstore/products_controller.rb
@@ -1,50 +1,77 @@
 module Backstore
-    class ProductsController < ApplicationController
+  class ProductsController < ApplicationController
+    def index
+      @q = Product.ransack(params[:q])
+      @products = @q.result(distinct: true)
+                     .includes(:images, :new_product, :used_product)
+                     .order(created_at: :desc)
+                     .page(params[:page]).per(20)
+    end
+
     def new
-        @product = Product.new
-        @product.build_new_product 
-        @product.build_used_product
-        # Inicializa un audio vacío opcional para el formulario
-        @product.used_product.build_audio
-        @product.images.build
+      @product = Product.new
+      @product.build_new_product 
+      @product.build_used_product
+      # Inicializa un audio vacío opcional para el formulario
+      @product.used_product.build_audio
+      @product.images.build
     end
 
     def create
-        @product = Product.new(product_params)
-        
-        # Lógica para limpiar asociaciones vacías según el estado seleccionado
-        if params[:state] == 'nuevo'
-            @product.used_product = nil 
-        else
-            @product.new_product = nil
-        end
+      @product = Product.new(product_params)
+      
+      # Lógica para limpiar asociaciones vacías según el estado seleccionado
+      if params[:state] == 'nuevo'
+        @product.used_product = nil 
+      else
+        @product.new_product = nil
+      end
 
-            if @product.save
+      if @product.save
         # Si se seleccionó una imagen como portada, actualizar cover_image_id
         if params[:cover_image_index].present?
-            cover_index = params[:cover_image_index].to_i
-            if @product.images[cover_index]
-                @product.update(cover_image_id: @product.images[cover_index].id)
-            end
+          cover_index = params[:cover_image_index].to_i
+          if @product.images[cover_index]
+            @product.update(cover_image_id: @product.images[cover_index].id)
+          end
         end
-        redirect_to products_path, notice: 'Producto creado exitosamente.'
-        else
-            render :new
-        end
+        redirect_to backstore_products_path, notice: 'Producto creado exitosamente.'
+      else
+        render :new
+      end
+    end
+
+    def edit
+      @product = Product.find(params[:id])
+    end
+
+    def update
+      @product = Product.find(params[:id])
+      if @product.update(product_params)
+        redirect_to backstore_products_path, notice: 'Producto actualizado.'
+      else
+        render :edit
+      end
+    end
+
+    def destroy
+      @product = Product.find(params[:id])
+      @product.destroy
+      redirect_to backstore_products_path, notice: 'Producto eliminado.'
     end
 
     private
 
     def product_params
-        params.require(:product).permit(
+      params.require(:product).permit(
         :name, :price, :author, :description, :category, :type, 
-        new_product_attributes: [:stock],
+        new_product_attributes: [:id, :stock],
         used_product_attributes: [
-            :id, 
-            audio_attributes: [:id, :url, :_destroy]
+          :id, 
+          audio_attributes: [:id, :url, :_destroy]
         ],
         images_attributes: [:id, :url, :_destroy]
-        )
+      )
     end
-    end
+  end
 end
